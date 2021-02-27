6fd6df35e7f45e1baa65dd6e02adb2d039aec7bb
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-02-27 14:27:06 +0900

    Update DFS_2606.py
    Add comment

--- BaekJoon/DFS_2606.py
+++ BaekJoon/DFS_2606.py
@@ -1,14 +1,14 @@
 import sys
 
 
-def dfs(graph, start_node):
-    visited = list()
-    need_visit = list()
+def dfs(graph, start_node):  # DFS 탐색 알고리즘.
+    visited = list()  # 탐색한 노드 (큐)
+    need_visit = list()  # 탐색해야할 노드 (스택)
     need_visit.append(start_node)
 
     while need_visit:
         node = need_visit.pop()
-        if node not in visited:
+        if node not in visited:  # 이미 간곳이 아니면 탐색하고 연결된 모든 노드들을 스택에 넣는다.
             visited.append(node)
             need_visit.extend(graph[node])
 
@@ -18,12 +18,12 @@ def dfs(graph, start_node):
 graph = dict()
 vertex_num = int(sys.stdin.readline())
 
-for i in range(1, vertex_num+1):
+for i in range(1, vertex_num+1):  # 입력받은 컴퓨터 수 만큼 vertex 생성.
     graph[i] = list()
 
 edge_num = int(sys.stdin.readline())
 
-for i in range(edge_num):
+for i in range(edge_num):  # 입력받은 edge 수 만큼 연결 관계를 표현
     key, value = map(int, sys.stdin.readline().split())
     graph[key].append(value)
     graph[value].append(key)
