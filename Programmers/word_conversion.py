bfb34f950bf339d7a8eb49d284ea13930d540646
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-09-27 23:28:02 +0900

    Update word_conversion.py

--- Programmers/word_conversion.py
+++ Programmers/word_conversion.py
@@ -2,25 +2,34 @@ from collections import deque
 
 
 def solution(begin, target, words):
-    answer = 0
-    visited = set([])
-    queue = deque()
-    queue.append(begin)
+    breadth = 0  # 그래프에서 몇번째 breadth에 속하는지.
+    queue = deque()  # 방문할 노드
+    visited = set([])  # 방문한 노드
+    same_breadth = []  # 같은 breadth에 속하는 노드들을 한번에 queue에서 제거후 보관.
 
     if target not in words:
-        return answer
+        return 0
 
-    for word in words:
-        for i in range(len(begin)):
-            count = 0
+    queue.append(begin)
+    visited.add(begin)
 
-            if begin[i] != word[i]:
-                count += 1
+    while queue:
+        while queue:  # 같은 breadth에 속하는 노드들을 한번에 queue에서 제거 후 보관.
+            same_breadth.append(queue.popleft())
+        breadth += 1  # 한번에 queue에서 제거할때 다음 breadth를 탐색.
+        for begin in same_breadth:
+            words = list(set(words) - visited)  # 방문 안한 노드.
 
-        if count == 1:
-            answer += 1
-            queue.append(word)
+            for word in words:
+                count = 0
+                for i in range(len(begin)):
 
-            if begin == target:
-                return answer
-    queue.popleft()
+                    if begin[i] != word[i]:
+                        count += 1
+
+                if count == 1:
+                    if word == target:
+                        return breadth
+                    queue.append(word)
+                    visited.add(word)
+    return 0
