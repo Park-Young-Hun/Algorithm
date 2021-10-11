5243382c061dd5a7f870148a75afccd55c75c306
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-10-11 21:29:44 +0900

    Create 마법사_상어와_토네이토_20057.py

---
+++ BaekJoon/마법사_상어와_토네이토_20057.py
@@ -0,0 +1,95 @@
+import sys
+
+# 모래 이동
+left = [[0, -2, 0.05], [1, -1, 0.1], [-1, -1, 0.1], [1, 0, 0.07], [-1, 0, 0.07], [2, 0, 0.02], [-2, 0, 0.02],
+        [1, 1, 0.01], [-1, 1, 0.01], [0, -1, 1]]
+right = [[r, -c, ratio] for r, c, ratio in left]
+up = [[c, r, ratio] for r, c, ratio in left]
+down = [[-c, r, ratio] for r, c, ratio in left]
+
+
+def blow(tr, tc, dir):
+    global ans
+    tmp = 0
+    if dir == 0:
+        for r, c, ratio in left:
+            if ratio == 1:
+                move_sand = sand[tr][tc] - tmp
+            else:
+                move_sand = int(sand[tr][tc] * ratio)
+                tmp += move_sand
+            if 0 <= tr + r < N and 0 <= tc + c < N:
+                sand[tr + r][tc + c] += move_sand
+            else:
+                ans += move_sand
+
+    if dir == 1:
+        for r, c, ratio in down:
+            if ratio == 1:
+                move_sand = sand[tr][tc] - tmp
+            else:
+                move_sand = int(sand[tr][tc] * ratio)
+                tmp += move_sand
+            if 0 <= tr + r < N and 0 <= tc + c < N:
+                sand[tr + r][tc + c] += move_sand
+            else:
+                ans += move_sand
+
+    if dir == 2:
+        for r, c, ratio in right:
+            if ratio == 1:
+                move_sand = sand[tr][tc] - tmp
+            else:
+                move_sand = int(sand[tr][tc] * ratio)
+                tmp += move_sand
+            if 0 <= tr + r < N and 0 <= tc + c < N:
+                sand[tr + r][tc + c] += move_sand
+            else:
+                ans += move_sand
+
+    if dir == 3:
+        for r, c, ratio in up:
+            if ratio == 1:
+                move_sand = sand[tr][tc] - tmp
+            else:
+                move_sand = int(sand[tr][tc] * ratio)
+                tmp += move_sand
+            if 0 <= tr + r < N and 0 <= tc + c < N:
+                sand[tr + r][tc + c] += move_sand
+            else:
+                ans += move_sand
+
+    sand[tr][tc] = 0
+
+
+def move(tr, tc):
+    # 좌 하 우 상
+    dir = 0
+    dr = [0, 1, 0, -1]
+    dc = [-1, 0, 1, 0]
+    while tr != 0 or tc != 0:
+        nr = tr + dr[dir]
+        nc = tc + dc[dir]
+        if not visit[nr][nc]:
+            visit[nr][nc] = 1
+            tr, tc = nr, nc
+            blow(tr, tc, dir)
+            dir += 1
+            if dir == 4:
+                dir = 0
+        else:
+            dir -= 1
+            if dir < 0:
+                dir = 3
+
+
+ans = 0
+N = int(sys.stdin.readline())
+sand = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+visit = [([0] * N) for _ in range(N)]
+tr, tc = N // 2, N // 2
+visit[tr][tc] = 1
+
+move(tr, tc)
+
+print(ans)
