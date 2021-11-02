cb37f3d852ef9d7949cb9154cd8755e615febd3e
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-02 14:54:39 +0900

    Update 연산자_끼워넣기_14888.py

--- BaekJoon/연산자_끼워넣기_14888.py
+++ BaekJoon/연산자_끼워넣기_14888.py
@@ -1,32 +1,54 @@
 import sys
 
-
-def solution(n, numbers, operators):
-    value = numbers[0]
-
-    numbers.sort()
-    for i in range(1, n):
-        if operators[1] != 0:
-            value -= numbers[i]
-            operators[1] -= 1
-        elif operators[3] != 0:
-            if value < 0 and numbers[1] < 0:
-                value = -value // -numbers[i]
-            elif value < 0:
-                value = -(-value // numbers[i])
-            else:
-                value //= numbers[i]
-            operators[3] -= 1
-        elif operators[0] != 0:
-            value += numbers[i]
-            operators[0] -= 1
-        else:
-            value *= numbers[i]
-            operators[2] -= 1
-    return value
-
-
 N = int(sys.stdin.readline())
-num = list(map(int, sys.stdin.readline().split()))
-operator = list(map(int, sys.stdin.readline().split()))
-print(solution(N, num, operator))
+numbers = list(map(int, sys.stdin.readline().split()))
+operators = list(map(int, sys.stdin.readline().split()))
+result = []
+
+
+def dfs(current_value, next_index, operator, operator_list):
+    if operator == "+":
+        current_value += numbers[next_index]
+        operator_list[0] -= 1
+    elif operator == "-":
+        current_value -= numbers[next_index]
+        operator_list[1] -= 1
+    elif operator == "*":
+        current_value *= numbers[next_index]
+        operator_list[2] -= 1
+    else:
+        if current_value < 0:
+            current_value = -(-current_value // numbers[next_index])
+        else:
+            current_value //= numbers[next_index]
+        operator_list[3] -= 1
+
+    if sum(operator_list) == 0:
+        result.append(current_value)
+        return
+    else:
+        for j in range(4):
+            if operator_list[j] != 0:
+                if j == 0:
+                    dfs(current_value, next_index + 1, "+", operator_list[:])
+                elif j == 1:
+                    dfs(current_value, next_index + 1, "-", operator_list[:])
+                elif j == 2:
+                    dfs(current_value, next_index + 1, "*", operator_list[:])
+                else:
+                    dfs(current_value, next_index + 1, "//", operator_list[:])
+
+
+for i in range(4):
+    if operators[i] != 0:
+        if i == 0:
+            dfs(numbers[0], 1, "+", operators[:])
+        elif i == 1:
+            dfs(numbers[0], 1, "-", operators[:])
+        elif i == 2:
+            dfs(numbers[0], 1, "*", operators[:])
+        else:
+            dfs(numbers[0], 1, "//", operators[:])
+
+print(max(result))
+print(min(result))
