6a850fef56828ad0b5b82c40232114494a13f916
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-10 18:32:04 +0900

    Create 괄호변환.py

---
+++ Programmers/괄호변환.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+
+def solution(p):
+    if p == "":
+        return ""
+
+    if verify(p):  # 올바른 괄호 문자열이면 그대로 return.
+        return p
+
+    p = list(p)
+    u = [p.pop(0)]
+    cnt = 1
+
+    while cnt != 0:  # 균형잡힌 괄호 문자열을 p로부터 분리.
+        item = p.pop(0)
+
+        if item == u[0]:
+            u.append(item)
+            cnt += 1
+        else:
+            u.append(item)
+            cnt -= 1
+
+    if verify(u):  # u가 올바른 괄호 문자열일 경우.
+        return "".join(u) + solution("".join(p))
+    else:  # u가 올바른 괄호 문자열이 아닌 경우.
+        u.pop(0)
+        u.pop()
+
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        return '(' + solution("".join(p)) + ')' + "".join(u)
+
+
+def verify(s):  # 올바른 괄호 문자열인지 판단.
+    queue = deque()
+
+    for i in s:
+        if i == '(':
+            queue.append(i)
+        else:
+            if queue:
+                queue.popleft()
+            else:
+                return False
+    return True
