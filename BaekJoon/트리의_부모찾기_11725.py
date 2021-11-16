48d57d3ed0bf21f96a6a0d27702aa129d601c424
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-16 23:13:24 +0900

    Update 트리의_부모찾기_11725.py

--- BaekJoon/트리의_부모찾기_11725.py
+++ BaekJoon/트리의_부모찾기_11725.py
@@ -1,5 +1,29 @@
 import sys
+from collections import defaultdict
+from collections import deque
 
 n = int(sys.stdin.readline())
 
-print(sys.stdin.readline().split())
+tree = defaultdict(list)
+
+for _ in range(n - 1):
+    edge = sys.stdin.readline().split()
+    tree[edge[0]].append(edge[1])
+    tree[edge[1]].append(edge[0])
+
+queue = deque('1')
+visited = [0] * n
+visited[0] = 1
+result = {str(i + 1): 0 for i in range(n)}
+
+while queue:
+    now = queue.popleft()
+    for i in tree[now]:
+        if visited[int(i) - 1] == 0:
+            result[i] = now
+            visited[int(i) - 1] = 1
+            queue.append(i)
+
+for key in result.keys():
+    if key != '1':
+        print(result[key])
