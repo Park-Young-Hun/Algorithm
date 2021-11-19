cd0a981287f87d63dd7f400c11f733a4561b58f1
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-19 16:43:22 +0900

    Create 기지국_설치.py

---
+++ Programmers/기지국_설치.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+
+def solution(n, stations, w):
+    answer = 0
+
+    for i in range(len(stations)+1):
+        if i == 0:
+            answer += ceil((stations[i]-w-1) / (2*w+1))
+        elif i == len(stations):
+            answer += ceil((n-stations[i-1]-w) / (2*w+1))
+        else:
+            answer += ceil((stations[i] - stations[i-1] - (2*w) - 1) / (2*w+1))
+    return answer
