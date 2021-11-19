49d3ab0e756cd1939d66c0363872c5e44a730f31
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-11-19 16:47:41 +0900

    Update 기지국_설치.py

--- Programmers/기지국_설치.py
+++ Programmers/기지국_설치.py
@@ -5,10 +5,11 @@ def solution(n, stations, w):
     answer = 0
 
     for i in range(len(stations)+1):
-        if i == 0:
+        if i == 0:  # 첫 아파트부터 첫번째 station까지의 구간
             answer += ceil((stations[i]-w-1) / (2*w+1))
-        elif i == len(stations):
+        elif i == len(stations):  # 마지막 station부터 마지막 아파트까지의 구간
             answer += ceil((n-stations[i-1]-w) / (2*w+1))
-        else:
+        else:  # station과 station 사이.
             answer += ceil((stations[i] - stations[i-1] - (2*w) - 1) / (2*w+1))
     return answer
+
