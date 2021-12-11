8143f34cceded3ebcff14dff1079a3687ef343f8
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-12-11 18:05:49 +0900

    Update 신규_아이디_추천.py

--- Programmers/신규_아이디_추천.py
+++ Programmers/신규_아이디_추천.py
@@ -1,5 +1,30 @@
 def solution(new_id):
     answer = ''
-    target = [i.lower() for i in new_id]
-    print(target)
+    target = "0123456789abcdefghijklmnopqrstuvwxyz-_."
+    new_id = list(new_id)
+    i = 0
+    j = 0
+    dot_cnt = 0
+
+    while i < len(new_id):
+        new_id[i] = new_id[i].lower()
+
+        if new_id[i] not in target:
+            new_id.remove(new_id[i])
+            i -= 1
+        i += 1
+
+    while j < len(new_id):
+        print(dot_cnt)
+        if new_id[j] == '.':
+            dot_cnt += 1
+            if dot_cnt > 1:
+                new_id.remove(new_id[j])
+            else:
+                j += 1
+        else:
+            dot_cnt = 0
+            j += 1
+    print(new_id)
     return answer
+
