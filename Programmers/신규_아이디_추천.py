5062047e9843a5b65a4b6daee3b7d4bc23b1a412
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-12-12 16:46:23 +0900

    Update 신규_아이디_추천.py

--- Programmers/신규_아이디_추천.py
+++ Programmers/신규_아이디_추천.py
@@ -1,12 +1,11 @@
 def solution(new_id):
-    answer = ''
     target = "0123456789abcdefghijklmnopqrstuvwxyz-_."
     new_id = list(new_id)
     i = 0
     j = 0
     dot_cnt = 0
 
-    while i < len(new_id):
+    while i < len(new_id):  # 1, 2단계
         new_id[i] = new_id[i].lower()
 
         if new_id[i] not in target:
@@ -14,17 +13,33 @@ def solution(new_id):
             i -= 1
         i += 1
 
-    while j < len(new_id):
-        print(dot_cnt)
+    while j < len(new_id):  # 3단계
+
         if new_id[j] == '.':
             dot_cnt += 1
             if dot_cnt > 1:
-                new_id.remove(new_id[j])
+                new_id.pop(j)
             else:
                 j += 1
         else:
             dot_cnt = 0
             j += 1
-    print(new_id)
-    return answer
 
+    if len(new_id) != 0 and new_id[0] == '.':  # 4단계
+        new_id.pop(0)
+    if len(new_id) != 0 and new_id[-1] == '.':
+        new_id.pop(-1)
+
+    if not new_id:  # 5단계
+        new_id.append('a')
+
+    if len(new_id) >= 16:  # 6단계
+        new_id = new_id[:15]
+    if len(new_id) != 0 and new_id[-1] == '.':
+        new_id.pop(-1)
+
+    if len(new_id) <= 2:  # 7단계
+        while len(new_id) < 3:
+            new_id.append(new_id[-1])
+
+    return "".join(new_id)
