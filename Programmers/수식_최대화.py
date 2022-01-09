b10c2f67a65ba7c8b386419d9a984881b62f3e85
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-01-09 22:00:37 +0900

    Update 수식_최대화.py

--- Programmers/수식_최대화.py
+++ Programmers/수식_최대화.py
@@ -4,14 +4,35 @@ from itertools import permutations
 def solution(expression):
     answer = 0
     operators = ["+", "*", "-"]
+
     cases = list(permutations(operators, 3))
 
-    print(cases)
-
     for case in cases:
+        temp_expression = expression
         for operator in case:
-            temp = ""
-            for letter in expression:
-                temp += letter
+            target = ""
+            calc_fg = False
+            for i in temp_expression[:]:
+                #print("target", target)
+                if i in operators and len(target) > 0:
+
+                    if calc_fg:
+                        print(temp_expression)
+                        temp_expression = temp_expression.replace(target, str(eval(target)))
+                        print(temp_expression)
+                        target = str(eval(target))
+                        calc_fg = False
+                    if i == operator:
+                        target += i
+                        calc_fg = True
+                    else:
+                        target = ""
+                else:
+                    target += i
+
+        # print(temp_expression)
 
     return answer
+
+
+solution("100-200*300-500+20")
