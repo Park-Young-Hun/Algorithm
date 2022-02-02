16ff078a04ebad4c6ef7ea6f8073ab4c09182635
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-02-02 19:20:35 +0900

    Update 순위_검색.py

--- Programmers/순위_검색.py
+++ Programmers/순위_검색.py
@@ -10,9 +10,10 @@ def solution(info, query):
     for i in range(len(info)):
         info[i] = info[i].split()
         info[i][-1] = int(info[i][-1])
-    info.sort(key=lambda x: x[4])
 
-    for i in info:
+    info.sort(key=lambda x: x[4])  # 추후에 binary search를 사용하기 위한 정렬.
+
+    for i in info:  # '-'를 포함하여 key-value 형태로 만듬.
         key_set = product([i[0], '-'], [i[1], '-'], [i[2], '-'], [i[3], '-'], )
 
         for j in key_set:
@@ -23,7 +24,7 @@ def solution(info, query):
         query[i] = query[i].split()
         query[i] = ["".join(query[i][:4]), int(query[i][-1])]
 
-    for i in query:
+    for i in query:  # query를 돌면서 info_dict의 value인 list를 이진 탐색함.
         answer.append(len(info_dict[i[0]]) - bisect_left(info_dict[i[0]], i[1]))
 
     return answer
