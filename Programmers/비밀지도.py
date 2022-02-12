44988590a24cb49ae448081a522fc6a3c5eb533a
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-02-12 21:52:56 +0900

    Update 비밀지도.py

--- Programmers/비밀지도.py
+++ Programmers/비밀지도.py
@@ -4,7 +4,7 @@ def solution(n, arr1, arr2):
     for i in range(n):
         row = bin(arr1[i] | arr2[i])[2:].replace('1', '#').replace('0', ' ')
 
-        if len(row) < n:
+        if len(row) < n:  # 비트 연산 후,앞자리에 있어 생략된 0의 갯수만큼 공백 추가해줌.
             space = " " * (n - len(row))
             row = space + row
         answer.append(row)
