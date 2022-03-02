150a89a2201c283bd62389700aa88d8f4b81a76d
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-03-03 07:18:17 +0900

    Update 톱니바퀴_14891.py

---
+++ BaekJoon/톱니바퀴_14891.py
@@ -0,0 +1,8 @@
+import sys
+
+gears = [list(map(int, sys.stdin.readline()[:-1])) for i in range(4)]
+
+rotates_num = int(sys.stdin.readline())
+
+rotates = [list(map(int, sys.stdin.readline().split())) for i in range(rotates_num)]
+
