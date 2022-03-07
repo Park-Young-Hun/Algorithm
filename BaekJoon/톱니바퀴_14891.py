70bab0bce08e05ba06ede72b5dfab824f104a2c2
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-03-07 15:51:17 +0900

    Update 톱니바퀴_14891.py

--- BaekJoon/톱니바퀴_14891.py
+++ BaekJoon/톱니바퀴_14891.py
@@ -7,21 +7,21 @@ rotates = [list(map(int, sys.stdin.readline().split())) for i in range(rotates_n
 
 
 def rotate(gear_num, direction, rotated):
-    if rotated[gear_num] == 1:
+    if rotated[gear_num] == 1:  # 한번의 시퀀스에서 이미 돌아간 톱니는 제외.
         return
 
-    rotated[gear_num] = 1
+    rotated[gear_num] = 1  # 톱니 돌아간거 체크.
 
-    if 0 < gear_num <= 3:
+    if 0 < gear_num <= 3:  # 대상 톱니의 왼쪽 톱니바퀴
         left = gear_num - 1
-        if gears[gear_num][6] != gears[left][2]:
+        if gears[gear_num][6] != gears[left][2]:  # 극이 다르면 반대방향으로 돌림.
             rotate(left, -direction, rotated)
-    if 0 <= gear_num < 3:
+    if 0 <= gear_num < 3:  # 대상 톱니의 오른쪽 톱니바퀴
         right = gear_num + 1
-        if gears[gear_num][2] != gears[right][6]:
+        if gears[gear_num][2] != gears[right][6]:  # 극이 다르면 반대방향으로 돌림.
             rotate(right, -direction, rotated)
 
-    if direction == 1:
+    if direction == 1:  # 시계 방향 또는 반시계 방향으로 회전.
         temp1 = gears[gear_num][-1]
         for i in range(len(gears[0])):
             temp2 = gears[gear_num][i]
