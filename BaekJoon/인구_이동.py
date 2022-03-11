754b417c48e00b70c2c2a1657c655cef79706389
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-03-11 17:41:09 +0900

    Create 인구_이동.py

---
+++ BaekJoon/인구_이동.py
@@ -0,0 +1,65 @@
+import sys
+from collections import deque
+
+N, L, R = map(int, sys.stdin.readline().split())
+table = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+target = [0]
+answer = 0
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+def bfs(x, y):
+    start = (x, y)
+
+    queue = deque()
+    queue.append(start)
+
+    local_visited = [start]
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                continue
+            if (nx, ny) in local_visited:
+                continue
+
+            if L <= abs(table[x][y] - table[nx][ny]) <= R:
+                local_visited.append((nx, ny))
+                queue.append((nx, ny))
+
+    if len(local_visited) == 1:
+        return []
+
+    return local_visited
+
+
+while target:
+    target = []
+    visited = []
+
+    for i in range(N):
+        for j in range(N):
+            if (i, j) not in visited:
+                new_target = bfs(i, j)
+                visited.extend(new_target)
+                target.append(new_target)
+    if target:
+        for i in target:
+            sum_val = 0
+
+            for x, y in i:
+                sum_val += table[x][y]
+
+            for x, y in i:
+                table[x][y] = sum_val // len(i)
+
+        answer += 1
+
+print(answer)
