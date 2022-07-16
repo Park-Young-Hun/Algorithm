0304837bd2781c7399034b2c8be76ed02f52c410
Young Hun Park <pyhpyh0670@gmail.com>
2022-07-16 16:23:53 +0900

    Update 미세먼지_안녕_17144.py

--- Python/BaekJoon/미세먼지_안녕_17144.py
+++ Python/BaekJoon/미세먼지_안녕_17144.py
@@ -4,13 +4,14 @@ from copy import deepcopy
 r, c, time = map(int, sys.stdin.readline().split())
 
 room = [list(map(int, sys.stdin.readline().split())) for _ in range(r)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
 cleaner = []
 answer = 0
 
 
 def spread(room):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
     new_room = deepcopy(room)
     for i in range(r):
         for j in range(c):
@@ -34,53 +35,48 @@ def spread(room):
     return new_room
 
 
-def clean(room):
-    ex_cell = 0
-    for j in range(1, c):
-        temp_cell = room[cleaner[0]][j]
-        room[cleaner[0]][j] = ex_cell
-        ex_cell = temp_cell
+def clean_up(room):
+    dx = [0, -1, 0, 1]
+    dy = [1, 0, -1, 0]
+    direct = 0
+    before = 0
+    x, y = cleaner[0], 1
+    while True:
+        nx = x + dx[direct]
+        ny = y + dy[direct]
+        if x == cleaner[0] and y == 0:
+            break
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            direct += 1
+            continue
+        room[x][y], before = before, room[x][y]
+        x = nx
+        y = ny
+    return room
 
-    for i in range(cleaner[0]-1, -1, -1):
-        temp_cell = room[i][c-1]
-        room[i][c-1] = ex_cell
-        ex_cell = temp_cell
 
-    for j in range(c-2, -1, -1):
-        temp_cell = room[0][j]
-        room[0][j] = ex_cell
-        ex_cell = temp_cell
-
-    for i in range(1, cleaner[0]):
-        temp_cell = room[i][0]
-        room[i][0] = ex_cell
-        ex_cell = temp_cell
-
-    ex_cell = 0
-    for j in range(1, c):
-        temp_cell = room[cleaner[1]][j]
-        room[cleaner[1]][j] = ex_cell
-        ex_cell = temp_cell
-
-    for i in range(cleaner[1] + 1, r):
-        temp_cell = room[i][c-1]
-        room[i][c-1] = ex_cell
-        ex_cell = temp_cell
-
-    for j in range(c - 2, -1, -1):
-        temp_cell = room[r-1][j]
-        room[r-1][j] = ex_cell
-        ex_cell = temp_cell
-
-    for i in range(r - 2, cleaner[1], -1):
-        temp_cell = room[i][0]
-        room[i][0] = ex_cell
-        ex_cell = temp_cell
+def clean_down(room):
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    direct = 0
+    before = 0
+    x, y = cleaner[1], 1
+    while True:
+        nx = x + dx[direct]
+        ny = y + dy[direct]
+        if x == cleaner[1] and y == 0:
+            break
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            direct += 1
+            continue
+        room[x][y], before = before, room[x][y]
+        x = nx
+        y = ny
     return room
 
 
 while time != 0:
-    room = clean(spread(room))
+    room = clean_down(clean_up(spread(room)))
     time -= 1
 
 for i in range(r):
