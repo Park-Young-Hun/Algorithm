412ee3234df5dc01c4a11e5944e5af19500f5121
Young Hun Park <pyhpyh0670@gmail.com>
2022-08-20 17:10:44 +0900

    Update 감시_15683.py

--- Python/BaekJoon/감시_15683.py
+++ Python/BaekJoon/감시_15683.py
@@ -1,7 +1,16 @@
 import sys
+from itertools import product
+from copy import deepcopy
 
 
-def zero_cnt(table):
+answer = []
+n, m = map(int, sys.stdin.readline().split())
+office = [sys.stdin.readline().split() for _ in range(n)]
+table = []
+cameras = []
+
+
+def zero_cnt():
     cnt = 0
     for i in range(n):
         for j in range(m):
@@ -10,9 +19,31 @@ def zero_cnt(table):
     return cnt
 
 
-n, m = map(int, sys.stdin.readline().split())
-office = [sys.stdin.readline().split() for _ in range(n)]
-cameras = []
+def dfs(x, y, direction):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    if direction == 'U':
+        nx = x + dx[0]
+        ny = y + dy[0]
+    elif direction == 'D':
+        nx = x + dx[1]
+        ny = y + dy[1]
+    elif direction == 'L':
+        nx = x + dx[2]
+        ny = y + dy[2]
+    elif direction == 'R':
+        nx = x + dx[3]
+        ny = y + dy[3]
+    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        return
+    if table[nx][ny] == '6':
+        return
+    if table[nx][ny] == '0':
+        table[nx][ny] = '#'
+    dfs(nx, ny, direction)
+    return
+
 
 for i in range(n):
     for j in range(m):
@@ -27,8 +58,67 @@ for i in range(n):
         elif office[i][j] == '5':
             cameras.append(['U'])
 
-cases = []
-print(cameras)
-for i in cameras:
-    for j in i:
-        
+cases = list(product(*cameras))
+
+for case in cases:
+    camera_num = 0
+    table = deepcopy(office)
+    for i in range(n):
+        for j in range(m):
+            if table[i][j] == '1':
+                dfs(i, j, case[camera_num])
+                camera_num += 1
+            elif table[i][j] == '2':
+                if case[camera_num] == 'U':
+                    dfs(i, j, 'U')
+                    dfs(i, j, 'D')
+                elif case[camera_num] == 'L':
+                    dfs(i, j, 'L')
+                    dfs(i, j, 'R')
+                camera_num += 1
+            elif table[i][j] == '3':
+                if case[camera_num] == 'U':
+                    dfs(i, j, 'U')
+                    dfs(i, j, 'R')
+                elif case[camera_num] == 'D':
+                    dfs(i, j, 'D')
+                    dfs(i, j, 'L')
+                elif case[camera_num] == 'L':
+                    dfs(i, j, 'L')
+                    dfs(i, j, 'U')
+                elif case[camera_num] == 'R':
+                    dfs(i, j, 'R')
+                    dfs(i, j, 'D')
+                camera_num += 1
+            elif table[i][j] == '4':
+                if case[camera_num] == 'U':
+                    dfs(i, j, 'U')
+                    dfs(i, j, 'L')
+                    dfs(i, j, 'R')
+                elif case[camera_num] == 'D':
+                    dfs(i, j, 'D')
+                    dfs(i, j, 'L')
+                    dfs(i, j, 'R')
+                elif case[camera_num] == 'L':
+                    dfs(i, j, 'L')
+                    dfs(i, j, 'U')
+                    dfs(i, j, 'D')
+                elif case[camera_num] == 'R':
+                    dfs(i, j, 'R')
+                    dfs(i, j, 'U')
+                    dfs(i, j, 'D')
+                camera_num += 1
+            elif table[i][j] == '5':
+                dfs(i, j, 'U')
+                dfs(i, j, 'D')
+                dfs(i, j, 'L')
+                dfs(i, j, 'R')
+                camera_num += 1
+    answer.append(zero_cnt())
+    # for i in table:
+    #     print(i)
+    # print(zero_cnt())
+    # print()
+
+print(min(answer))
+
