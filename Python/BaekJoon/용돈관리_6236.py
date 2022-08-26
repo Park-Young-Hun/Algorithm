336c79a4dba64fcc07d3624246befeff3972b68d
Young Hun Park <pyhpyh0670@gmail.com>
2022-08-26 13:28:04 +0900

    Update 용돈관리_6236.py

--- Python/BaekJoon/용돈관리_6236.py
+++ Python/BaekJoon/용돈관리_6236.py
@@ -7,14 +7,27 @@ moneys = [int(sys.stdin.readline()) for _ in range(n)]
 sorted_moneys = sorted(moneys)
 
 
-def simulate(k, moneys) :
+def simulate(k, money_list):
     pocket = 0
     cnt = 0
-    for money in moneys:
+    for money in money_list:
+        if cnt >= m:
+            return False
         if pocket >= money:
             pocket -= money
             continue
-        pocket = k
-        cnt += 1
+        while money > 0:
+            pocket = k
+            cnt += 1
+            money -= k
+    if cnt > m:
+        return False
+    else:
+        return True
 
 
+def binary_search(money_list):
+    left = money_list[0]
+    right = money_list[-1]
+
+    
