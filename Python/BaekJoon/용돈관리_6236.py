eff65531e15e372b715f93e0d3b22a3ccde82734
Young Hun Park <pyhpyh0670@gmail.com>
2022-09-01 18:11:01 +0900

    Update 용돈관리_6236.py

--- Python/BaekJoon/용돈관리_6236.py
+++ Python/BaekJoon/용돈관리_6236.py
@@ -1,45 +1,40 @@
 import sys
-from math import ceil
 
 
 n, m = map(int, sys.stdin.readline().split())
 
 moneys = [int(sys.stdin.readline()) for _ in range(n)]
-sorted_moneys = sorted(moneys)
 
 
-def simulate(k, money_list):
-    pocket = 0
-    cnt = 0
+def simulate(k, money_list):  # M번의 인출 횟수 내에 N일 동안 생활이 가능한 지 시뮬레이션.
+    if max(money_list) > k:
+        return False
+    pocket = k
+    cnt = 1
     for money in money_list:
-        if cnt >= m:
-            return False
-        if pocket >= money:
-            pocket -= money
-            continue
-        while money > 0:
+        if pocket < money:
             pocket = k
             cnt += 1
-            money -= k
+        pocket -= money
     if cnt > m:
         return False
-    else:
-        return True
+    return True
 
 
-def binary_search(money_list):
-    left = min(money_list)
-    right = max(money_list)
+def binary_search(money_list):  # k의 최솟값을 찾기 위한 이분탐색.
+    start = min(money_list)
+    end = sum(money_list)
+    target = end
 
-    while left < right:
-        mid = (left + right) // 2
-        print(mid)
+    while start <= end:
+        mid = (start + end) // 2
 
         if simulate(mid, money_list):
-            right = mid - 1
+            end = mid - 1
+            target = mid  # 현재 값보다 작은 값을 탐색할 때만 target에 저장.
         else:
-            left = mid + 1
-    return mid
+            start = mid + 1
+    return target
 
 
 print(binary_search(moneys))
