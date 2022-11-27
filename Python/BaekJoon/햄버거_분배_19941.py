641e0f32329409c0a475be0b19d17f93cea19604
Young Hun Park <pyhpyh0670@gmail.com>
2022-11-27 10:52:30 +0900

    Create 햄버거_분배_19941.py

---
+++ Python/BaekJoon/햄버거_분배_19941.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+table = list(sys.stdin.readline())
+hamburgers = deque()
+people = deque()
+
+for i in range(len(table)):
+    if table[i] == 'H':
+        hamburgers.append(i)
+    else:
+        people.append(i)
+print(hamburgers)
+print(people)
+
+while hamburgers and people:
+    person = people.popleft()
+
+    for hamburger in hamburgers:
+        if abs(hamburger-person) <= k:
+            hamburgers.remove(hamburger)
