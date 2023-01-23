e750de4a4bbd33eac8c2a07ce6a0674bcb9442ff
Young Hun Park <pyhpyh0670@gmail.com>
2023-01-22 22:25:02 -0800

    Update PronouncePassword_4659.java

--- Java/src/BaekJoon/PronouncePassword_4659.java
+++ Java/src/BaekJoon/PronouncePassword_4659.java
@@ -1,8 +1,31 @@
 package BaekJoon;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringTokenizer;
+
 public class PronouncePassword_4659 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
 
+        List<String> passwords = new ArrayList<>();
+
+        while (true) {
+            String password = br.readLine();
+
+            if (password.equals("end")) {
+                break;
+            }
+            passwords.add(password);
+        }
+        bw.write(String.valueOf(passwords));
+        bw.flush();
     }
+
+    public String solution
 }
