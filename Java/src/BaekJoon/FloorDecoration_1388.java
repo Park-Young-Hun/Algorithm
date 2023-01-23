1d9e39d024ab1e617de6b6306ac846ec2b5e6fe2
Young Hun Park <pyhpyh0670@gmail.com>
2023-01-22 22:24:57 -0800

    Update FloorDecoration_1388.java

--- Java/src/BaekJoon/FloorDecoration_1388.java
+++ Java/src/BaekJoon/FloorDecoration_1388.java
@@ -10,6 +10,7 @@ public class FloorDecoration_1388 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         String map[][] = new String[n][m];
