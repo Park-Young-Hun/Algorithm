7599be4fb2aa29206ff030c0243a5a4865c970fd
Young Hun Park <pyhpyh0670@gmail.com>
2023-02-17 15:31:56 +0900

    Update BestAlbum.java

--- Java/src/Programmers/BestAlbum.java
+++ Java/src/Programmers/BestAlbum.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class BestAlbum {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
 
         HashMap<String, List<List<Integer>>> genreMap = new HashMap<>();
+        HashMap<Integer, String> playMap = new HashMap<>();
+        List<Integer> playSums = new ArrayList<>();
 
         for (int i=0; i<genres.length; i++) {
             if (!genreMap.containsKey(genres[i])) {
@@ -21,7 +23,30 @@ public class BestAlbum {
             song.add(plays[i]);
             genreMap.get(genres[i]).add(song);
         }
-        System.out.println(genreMap);
-        return answer;
+
+        for (String key : genreMap.keySet()) {
+            Integer playSum = 0;
+
+            genreMap.get(key).sort((s1, s2) -> s1.get(1).equals(s2.get(1)) ? s1.get(0) - s2.get(0) : s2.get(1) - s1.get(1));
+
+            for (List<Integer> songs : genreMap.get(key)) {
+                playSum += songs.get(1);
+            }
+            playMap.put(playSum, key);
+            playSums.add(playSum);
+        }
+        playSums.sort(Collections.reverseOrder());
+
+        for (Integer playSum : playSums) {
+            String genre = playMap.get(playSum);
+            List<List<Integer>> songs = genreMap.get(genre);
+
+            answer.add(songs.get(0).get(0));
+
+            if (songs.size() > 1) {
+                answer.add(songs.get(1).get(0));
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
