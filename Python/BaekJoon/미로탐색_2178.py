26977a0114f63d5d68801ae390b5808b8843fd5a
Young Hun Park <pyhpyh0670@gmail.com>
2023-03-03 14:12:05 +0900

    Update 미로탐색_2178.py

--- Python/BaekJoon/미로탐색_2178.py
+++ Python/BaekJoon/미로탐색_2178.py
@@ -1,35 +1,38 @@
 import sys
 
 
-def solution(N, M, maze):
-    cnt = -1
-    answer = []
-    stack = [(0, 0)]
+def dfs(start):
+    global cnt
     delta_row = [1, -1, 0, 0]
     delta_col = [0, 0, 1, -1]
-    visited = [[False] * M for _ in range(N)]
 
-    while stack:
-        row, col = stack.pop()
-        visited[row][col] = True
-        cnt += 1
+    row, col = start
+    visited[row][col] = True
+    cnt += 1
 
-        if row == N-1 and col == M-1:
-            answer.append(cnt)
+    if row == N-1 and col == M-1:
+        answer.append(cnt)
+        return
+
+    for i in range(4):
+        next_row = row + delta_row[i]
+        next_col = col + delta_col[i]
+
+        if 0 <= next_row < N and 0 <= next_col < M and maze[next_row][next_col] == 1 and not visited[next_row][next_col]:
+            dfs((next_row, next_col))
+            visited[next_row][next_col] = False
             cnt -= 1
-            visited[row][col] = False
-
-        for i in range(4):
-            next_row = row + delta_row[i]
-            next_col = col + delta_col[i]
-
-            if 0 <= next_row < N and 0 <= next_col < M and maze[next_row][next_col] == 1 and not visited[next_row][next_col]:
-                stack.append((next_row, next_col))
+    return
 
 
 N, M = map(int, sys.stdin.readline().split())
 maze = [list(map(int, list(sys.stdin.readline())[:-1])) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+answer = []
 
-print(maze)
+global cnt
+cnt = 0
 
-solution(N, M, maze)
+dfs((0, 0))
+
+print(min(answer))
