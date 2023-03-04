e637a982cf0edb6fb225e17c18f691176ce9dfb4
Young Hun Park <pyhpyh0670@gmail.com>
2023-03-04 18:44:05 +0900

    Update 미로탐색_2178.py

--- Python/BaekJoon/미로탐색_2178.py
+++ Python/BaekJoon/미로탐색_2178.py
@@ -1,38 +1,38 @@
 import sys
+from collections import deque
 
 
-def dfs(start):
-    global cnt
+def solution(N, M, maze):
+    breadth = 1
     delta_row = [1, -1, 0, 0]
     delta_col = [0, 0, 1, -1]
+    visited = [[False] * M for _ in range(N)]
+    q = deque([(0, 0)])
 
-    row, col = start
-    visited[row][col] = True
-    cnt += 1
+    while q:
+        for _ in range(len(q)):
+            row, col = q.popleft()
 
-    if row == N-1 and col == M-1:
-        answer.append(cnt)
-        return
+            if row == N - 1 and col == M - 1:
+                return breadth
 
-    for i in range(4):
-        next_row = row + delta_row[i]
-        next_col = col + delta_col[i]
+            for i in range(4):
+                next_row = row + delta_row[i]
+                next_col = col + delta_col[i]
 
-        if 0 <= next_row < N and 0 <= next_col < M and maze[next_row][next_col] == 1 and not visited[next_row][next_col]:
-            dfs((next_row, next_col))
-            visited[next_row][next_col] = False
-            cnt -= 1
-    return
+                if next_row < 0 or next_row > N-1 or next_col < 0 or next_col > M-1:
+                    continue
+
+                if visited[next_row][next_col]:
+                    continue
+
+                if maze[next_row][next_col] == 1:
+                    q.append((next_row, next_col))
+                    visited[next_row][next_col] = True
+        breadth += 1
 
 
 N, M = map(int, sys.stdin.readline().split())
-maze = [list(map(int, list(sys.stdin.readline())[:-1])) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-answer = []
+maze = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 
-global cnt
-cnt = 0
-
-dfs((0, 0))
-
-print(min(answer))
+print(solution(N, M, maze))
