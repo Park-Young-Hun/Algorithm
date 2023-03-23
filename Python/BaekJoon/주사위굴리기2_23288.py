ed11dc56ae013dbdb8d33781d85e3a052ab30d73
Young Hun Park <pyhpyh0670@gmail.com>
2023-03-23 14:37:34 +0900

    Update 주사위굴리기2_23288.py

--- Python/BaekJoon/주사위굴리기2_23288.py
+++ Python/BaekJoon/주사위굴리기2_23288.py
@@ -49,15 +49,70 @@ class Dice:
         self.right = temp_top
 
 
+def decide_direction(direction, bottom, target):
+    if bottom > target:
+        direction = (direction + 1) % 4
+    elif bottom < target:
+        direction -= 1
+        if direction < 0:
+            direction += 4
+    return direction
+
+
+def calc_score(target: int, pos: tuple):
+    cnt = 0
+    visited = [[False] * m for _ in range(n)]
+    delta = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    stack = [pos]
+    visited[pos[0]][pos[1]] = True
+
+    while stack:
+        row, col = stack.pop()
+        cnt += 1
+
+        for i in range(4):
+            delta_row, delta_col = delta[i]
+
+            new_row = row + delta_row
+            new_col = col + delta_col
+
+            if new_row < 0 or new_row >= n or new_col < 0 or new_col >= m:
+                continue
+
+            if not visited[new_row][new_col] and grid[new_row][new_col] == target:
+                visited[new_row][new_col] = True
+                stack.append((new_row, new_col))
+
+    return cnt * target
+
+
 def solution(n, m, k, grid):
+    answer = 0
+    direction = 0
+    row, col = 0, 0
     dice = Dice(2, 5, 4, 3, 1, 6)
     delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    direct = 0
-    row, col = 0, 0
 
     while k > 0:
-        delta_row, delta_col = delta[direct]
-        dice.move(direct)
+        delta_row, delta_col = delta[direction]
+        new_row = row + delta_row
+        new_col = col + delta_col
+
+        if new_row < 0 or new_row >= n or new_col < 0 or new_col >= m:  # 맵을 벗어나면 반대 방향으로 전환.
+            direction = (direction + 2) % 4
+            continue
+
+        row = new_row
+        col = new_col
+
+        dice.move(direction)  # 주사위 이동.
+
+        answer += calc_score(grid[row][col], (row, col))  # 점수 계산.
+
+        direction = decide_direction(direction, dice.bottom, grid[row][col])  # 다음 방향 결정.
+        k -= 1
+
+    return answer
 
 
 n, m, k = map(int, sys.stdin.readline().split())
