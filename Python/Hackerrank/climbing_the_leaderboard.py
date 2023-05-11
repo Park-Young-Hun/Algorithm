c3dc490187e7408c2968f684c74f10277f66de49
Young Hun Park <pyhpyh0670@gmail.com>
2023-05-11 18:06:25 +0900

    Create climbing_the_leaderboard.py

---
+++ Python/Hackerrank/climbing_the_leaderboard.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from bisect import bisect_left
+
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    answer = []
+
+    for score in player:
+        print(len(ranked) - bisect_left(list(reversed(ranked)), score))
+    return answer
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
