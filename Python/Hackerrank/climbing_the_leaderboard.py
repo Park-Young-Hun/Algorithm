1a7faa16016bb790ce9772cc221709f1b45c595c
Young Hun Park <pyhpyh0670@gmail.com>
2023-05-12 17:30:12 +0900

    Update climbing_the_leaderboard.py

--- Python/Hackerrank/climbing_the_leaderboard.py
+++ Python/Hackerrank/climbing_the_leaderboard.py
@@ -16,13 +16,39 @@ from bisect import bisect_left
 #  1. INTEGER_ARRAY ranked
 #  2. INTEGER_ARRAY player
 #
+class Participant:
+    def __init__(self, score, rank):
+        self.score = score
+        self.rank = rank
+
 
 def climbingLeaderboard(ranked, player):
     # Write your code here
     answer = []
+    participants = []
+    rank = 1
+
+    for i in range(len(ranked)):
+        if i > 0 and ranked[i - 1] > ranked[i]:
+            rank += 1
+        participants.append(Participant(ranked[i], rank))
+
+    r_ranked = list(reversed(ranked))
 
     for score in player:
-        print(len(ranked) - bisect_left(list(reversed(ranked)), score))
+        insert_idx = len(ranked) - bisect_left(r_ranked, score)
+
+        if insert_idx == 0:
+            answer.append(1)
+            continue
+
+        target = participants[insert_idx - 1]
+
+        if score < target.score:
+            answer.append(target.rank + 1)
+        else:
+            answer.append(target.rank)
+
     return answer
 
 
