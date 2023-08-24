13f4969a455ba2aff5d5133d2a6ab7e97406a7fd
Young Hun Park <pyhpyh0670@gmail.com>
2023-08-24 19:26:48 +0900

    Update 할인행사.py

--- Python/Programmers/할인행사.py
+++ Python/Programmers/할인행사.py
@@ -1,39 +1,19 @@
-from collections import deque, defaultdict
-
-
 def solution(want, number, discount):
     answer = 0
+    wanted_items = []
     MAX_WINDOW_LEN = 10
-    window = deque()
-    discount = deque(discount)
-    want_cnts = defaultdict(int)
 
     for i in range(len(want)):
-        want_cnts[want[i]] = number[i]
+        for _ in range(number[i]):
+            wanted_items.append(want[i])
 
-    for _ in range(MAX_WINDOW_LEN):
-        item = discount.popleft()
-        window.append(item)
+    wanted_items.sort()
 
-        if want_cnts[item] > 0:
-            want_cnts[item] -= 1
+    for i in range(len(discount) - MAX_WINDOW_LEN + 1):
+        window = discount[i:i + MAX_WINDOW_LEN]
+        window.sort()
 
-    if sum(want_cnts.values()) == 0:
-        answer += 1
-
-    while discount:
-        item = window.popleft()
-
-        if want_cnts[item] > 0:
-            want_cnts[item] += 1
-
-        new_item = discount.popleft()
-        window.append(new_item)
-
-        if want_cnts[new_item] > 0:
-            want_cnts[new_item] -= 1
-
-        if sum(want_cnts.values()) == 0:
+        if window == wanted_items:
             answer += 1
 
     return answer
