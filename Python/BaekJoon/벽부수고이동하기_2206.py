421c044bef3a3936fd0457ce8e2c422a07025809
Young Hun Park <pyhpyh0670@gmail.com>
2023-08-30 21:22:37 +0900

    Update 벽부수고이동하기_2206.py

--- Python/BaekJoon/벽부수고이동하기_2206.py
+++ Python/BaekJoon/벽부수고이동하기_2206.py
@@ -2,22 +2,38 @@ import sys
 from collections import deque
 
 
-class Node:
+def solution(n, m, graph):
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    visited[0][0][0] = 1
 
-    def __init__(self, row, col, depth):
-        self.row = row
-        self.col = col
-        self.depth = depth
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
 
+    queue = deque()
+    queue.append((0, 0, 0))
 
-def solution(n, m, grid):
-    delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    start = Node(0, 0, 0)
-    way_queue = deque([start])
-    wall_queue = deque()
+    while queue:
+        a, b, c = queue.popleft()
 
-    while way_queue or wall_queue:
-        way_queue.popleft()
+        if a == n - 1 and b == m - 1:
+            return visited[a][b][c]
+
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            # 다음 이동할 곳이 벽이고, 벽파괴기회를 사용하지 않은 경우
+            if graph[nx][ny] == 1 and c == 0:
+                visited[nx][ny][1] = visited[a][b][0] + 1
+                queue.append((nx, ny, 1))
+            # 다음 이동할 곳이 벽이 아니고, 아직 한 번도 방문하지 않은 곳이면
+            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
+                visited[nx][ny][c] = visited[a][b][c] + 1
+                queue.append((nx, ny, c))
+    return -1
 
 
 n, m = map(int, sys.stdin.readline().split())
