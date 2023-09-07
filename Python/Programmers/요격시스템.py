f7e360110cc8d553a27e4ccf1cb296dd9e393845
Young Hun Park <pyhpyh0670@gmail.com>
2023-09-07 09:03:27 +0900

    Create 요격시스템.py

---
+++ Python/Programmers/요격시스템.py
@@ -0,0 +1,12 @@
+def solution(targets):
+    answer = 0
+    targets.sort(key=lambda x: [x[1], x[0]])
+
+    e = 0
+
+    for target in targets:
+        if target[0] >= e:
+            answer += 1
+            e = target[1]
+
+    return answer
