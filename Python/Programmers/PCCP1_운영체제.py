c28ca9504a1bb56e11ca43ff53a38c603f11889c
Young Hun Park <pyhpyh0670@gmail.com>
2023-09-07 09:02:37 +0900

    Update PCCP1_운영체제.py

--- Python/Programmers/PCCP1_운영체제.py
+++ Python/Programmers/PCCP1_운영체제.py
@@ -14,22 +14,20 @@ def solution(program):
 
     while ready_q:
         rank, arrived_time, run_time = ready_q.popleft()
-        rank_wait_times[rank] += time - arrived_time
 
         if time < arrived_time:
             time += arrived_time - time
 
-        while time < arrived_time + run_time:
-            if ready_q and ready_q[0][1] == time:
-                target = ready_q.popleft()
-                heapq.heappush(wait_q, (target[1], target[0], target[2]))
-            time += 1
-        print(time)
+        rank_wait_times[rank] += time - arrived_time
+
+        time += run_time
+
+        while ready_q and ready_q[0][1] <= time:
+            heapq.heappush(wait_q, ready_q.popleft())
 
         if wait_q:
             next_job = heapq.heappop(wait_q)
-            arrived_time, rank, run_time = next_job
-            ready_q.appendleft((rank, arrived_time, run_time))
+            ready_q.appendleft(next_job)
 
     answer[0] = time
 
